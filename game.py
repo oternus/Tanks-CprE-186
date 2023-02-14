16e7ab94ff3c5b69aed2e9471c192432e0d89c04
SneakyTurtleII <114328925+SneakyTurtleII@users.noreply.github.com>
2023-02-14 12:47:51 -0600

    Add files via upload

---
+++ game.py
@@ -0,0 +1,28 @@
+import pygame    
+
+# initializing the window
+pygame.init() # initscr(); 
+screen = pygame.display.set_mode((800,800))
+logo = pygame.image.load("logo.png") # image.png is the logo
+pygame.display.set_icon(logo) # refresh();
+pygame.display.set_caption("My Game") # name of program
+
+image = pygame.image.load("logo.png") # loads the image
+
+running = True
+x = 0
+y = 0
+
+if running:
+    print("User started the game")
+
+while running:    
+    for event in pygame.event.get():
+        screen.blit(image, (x,y))
+        if pygame.key.get_pressed()[pygame.K_s]: # if user presses s
+            y -= 10 # moves down
+            print(y)
+        if event.type == pygame.QUIT: # alt f4 or top left X  
+            running = False
+            print("User quit the game")
+    pygame.display.flip() # refresh();
