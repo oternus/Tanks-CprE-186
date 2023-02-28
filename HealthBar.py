7507db510eb49aaf9caaf1d2b94306dbf72dbce3
oternus3 <125397697+oternus3@users.noreply.github.com>
2023-02-28 12:36:59 -0600

    Update HealthBar.py

--- HealthBar.py
+++ HealthBar.py
@@ -1,49 +1,67 @@
 import pygame
 
-# Initialize Pygame
 pygame.init()
 
-# Set up the screen
-screen_width = 800
-screen_height = 600
+# Set up the display
+screen_width = 600
+screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Health Bars")
 
-# Set up the font
+# Define some colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+# Set up the fonts
 font = pygame.font.Font(None, 30)
 
-# Set up the health bar
-health = 100
-health_bar_width = 200
-health_bar_height = 20
-health_bar_border_width = 2
-health_bar_color = (255, 0, 0)
-health_bar_background_color = (255, 255, 255)
-health_bar_x = screen_width - health_bar_width - 10
-health_bar_y = 10
+# Set up the initial health values
+player_health = 100
+enemy_health = 100
 
-# Game loop
+# Define the function to draw the health bars
+def draw_health_bars():
+    # Draw the player's health bar
+    player_bar_width = 200
+    player_bar_height = 20
+    player_health_percent = max(player_health / 100, 0)
+    player_bar_filled_width = player_bar_width * player_health_percent
+    player_bar_rect = pygame.Rect(50, 50, player_bar_filled_width, player_bar_height)
+    pygame.draw.rect(screen, green, player_bar_rect)
+    player_bar_outline_rect = pygame.Rect(50, 50, player_bar_width, player_bar_height)
+    pygame.draw.rect(screen, white, player_bar_outline_rect, 2)
+    player_health_text = font.render(f"Player Health: {player_health}", True, white)
+    screen.blit(player_health_text, (50, 25))
+    
+    # Draw the enemy's health bar
+    enemy_bar_width = 200
+    enemy_bar_height = 20
+    enemy_health_percent = max(enemy_health / 100, 0)
+    enemy_bar_filled_width = enemy_bar_width * enemy_health_percent
+    enemy_bar_rect = pygame.Rect(screen_width - enemy_bar_filled_width - 50, 50, enemy_bar_filled_width, enemy_bar_height)
+    pygame.draw.rect(screen, red, enemy_bar_rect)
+    enemy_bar_outline_rect = pygame.Rect(screen_width - enemy_bar_width - 50, 50, enemy_bar_width, enemy_bar_height)
+    pygame.draw.rect(screen, white, enemy_bar_outline_rect, 2)
+    enemy_health_text = font.render(f"Enemy Health: {enemy_health}", True, white)
+    screen.blit(enemy_health_text, (screen_width - enemy_health_text.get_width() - 50, 25))
+
+# Run the game loop
 running = True
 while running:
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # Clear the screen
-    screen.fill((255, 255, 255))
-
-    # Draw the health bar background
-    pygame.draw.rect(screen, health_bar_background_color, (health_bar_x, health_bar_y, health_bar_width, health_bar_height))
-
-    # Draw the health bar
-    pygame.draw.rect(screen, health_bar_color, (health_bar_x, health_bar_y, health / 100 * health_bar_width, health_bar_height))
-
-    # Draw the health text
-    health_text = font.render(f"Health: {health}", True, (0, 0, 0))
-    screen.blit(health_text, (10, 10))
-
-    # Update the screen
+    
+    # Update the game state
+    player_health -= 0.1
+    enemy_health -= 0.05
+    
+    # Draw the game
+    screen.fill(black)
+    draw_health_bars()
     pygame.display.update()
 
-# Quit Pygame
 pygame.quit()
