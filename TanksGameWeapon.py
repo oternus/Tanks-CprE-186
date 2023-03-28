b9fa38a298faca605040e50e81096663f0ae5c30
oternus3 <125397697+oternus3@users.noreply.github.com>
2023-03-28 13:47:55 -0500

    Add files via upload
    Weapons prototype for game.

---
+++ TanksGameWeapon.py
@@ -0,0 +1,12 @@
+#player weapon
+player_weapon = pygame.image.load("shotgun.png").convert()
+player_weapon.set_colorkey((255,255,255))
+
+#THIS GOES IN PLAYER CLASS
+
+
+#enemy weapon
+enemy_weapon = pygame.image.load("shotgun.png").convert()
+enemy_weapon.set_colorkey((255,255,255))\
+
+#THIS GOES IN EMEMY CLASS
