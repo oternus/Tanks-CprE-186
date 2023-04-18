5ce405caa77a1556e5afc47225f0a1cecdc51bbb
SneakyTurtleII <114328925+SneakyTurtleII@users.noreply.github.com>
2023-04-18 12:36:12 -0500

    Update endScreen.py

--- endScreen.py
+++ endScreen.py
@@ -3,8 +3,10 @@ import Button
 
 pygame.init()
 
-SCREEN_WIDTH = 1000
+SCREEN_WIDTH = 1300
 SCREEN_HEIGHT = 650 
+BUTTON_WIDTH = 600
+BUTTON_HEIGHT = 75
 myfont = pygame.font.SysFont("Arial", 30)
 #winner should be TANK1 or TANK2 as a string
 
@@ -17,12 +19,12 @@ def end_Screen(winner):
     orange = (255,165,0)
     white = (255,255, 255)
 
-    winnerBox = Button.Button(0.40 * SCREEN_WIDTH - 75, 0.10 * SCREEN_HEIGHT, winner + " IS THE WINNER", 30, white, black, 1, width=350, height=100, border=2, border_color=(white))
+    winnerBox = Button.Button(0.5 * SCREEN_WIDTH - 300, 0.00 * SCREEN_HEIGHT + 50, f"{winner} WON", 30, white, black, 1, width=BUTTON_WIDTH, height=BUTTON_HEIGHT+50, border=2, border_color=(white))
 
     # create buttons
-    start_button = Button.Button(0.5 * SCREEN_WIDTH - 75, 0.33 * SCREEN_HEIGHT, "PLAY AGAIN?", 25, cyan, black, 1, width=150, height=50, border=2, border_color=(cyan))
-    quit_button = Button.Button(0.5 * SCREEN_WIDTH - 75, 0.66 * SCREEN_HEIGHT, "QUIT", 25, red, black, 1, width=150, height=50, border=2, border_color=(red))
-    settings_button = Button.Button(0.5 * SCREEN_WIDTH - 75, 0.496 * SCREEN_HEIGHT, "SETTINGS", 25, orange, black, 1, width=150, height=50, border=2, border_color=(orange))
+    start_button = Button.Button(0.5 * SCREEN_WIDTH - 300, 0.25 * SCREEN_HEIGHT + 100, "PLAY AGAIN", 25, cyan, black, 1, width=BUTTON_WIDTH, height=BUTTON_HEIGHT, border=2, border_color=(cyan))
+    quit_button = Button.Button(0.5 * SCREEN_WIDTH - 300, 0.55 * SCREEN_HEIGHT + 100, "QUIT", 25, red, black, 1, width=BUTTON_WIDTH, height=BUTTON_HEIGHT, border=2, border_color=(red))
+    settings_button = Button.Button(0.5 * SCREEN_WIDTH - 300, 0.40 * SCREEN_HEIGHT + 100, "SETTINGS", 25, orange, black, 1, width=BUTTON_WIDTH, height=BUTTON_HEIGHT, border=2, border_color=(orange))
 
     running = True
 
@@ -44,9 +46,12 @@ def end_Screen(winner):
         # check if buttons were clicked
         if start_action:
             print("Play again button clicked")
+            running = False
+            return 1
         elif setting_action:
             print("Settings button clicked")
         elif quit_action:
             running = False
+            return 0
 
         pygame.display.flip()
