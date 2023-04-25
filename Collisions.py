e69f15f5834c9d87d794548830bbb4b94a290304
Ali <114328925+SneakyTurtleII@users.noreply.github.com>
2023-04-25 12:19:46 -0500

    Update Collisions.py

--- Collisions.py
+++ Collisions.py
@@ -1,4 +1,4 @@
 from Conventions import *
 
 def tank_hit_detection(distance_x, distance_y):
-    return ((distance_x < (TANK_HITBOX_WIDTH)) and (distance_y < (TANK_HITBOX_HEIGHT)))
+    return ((abs(distance_x) < (TANK_HITBOX_WIDTH)) and (abs(distance_y) < (TANK_HITBOX_HEIGHT)))
